b71865f2f09f5bb2202179f65b0318d112b9a66d
nithin10105 <nithin864@gmail.com>
2025-08-23 16:08:08 +0530

    save this files

---
+++ first-intro-to-git/README.md
@@ -0,0 +1,1 @@
+this is a file under the first to git
---
+++ first-intro-to-git/basic-syntax.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string name="hi i am nithin currently pursing btech degree fron nit Allahabad";
+    cout << name << endl;
+    cout << "this is testing the file name:";
+}
